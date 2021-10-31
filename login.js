047e87ab73edf97d1aefdc85120e52d204ba3647
“low-key-person” <2568388237@qq.com>
2021-10-31 19:45:22 +0800

    a

---
+++ login.js
@@ -0,0 +1,11 @@
+function check() {
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    if (name == "1" && pass == "1") {
+        alert("登入成功");
+        window.document.f.action = "./Update/index.html";
+        window.document.f.submit();
+    } else {
+        alert("用户名或密码错误");
+    }
+}
