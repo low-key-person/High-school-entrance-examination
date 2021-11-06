5538679be3bca826f68922fde4049aa4a606900e
“low-key-person” <2568388237@qq.com>
2021-11-06 17:41:15 +0800

    a

--- login.js
+++ login.js
@@ -4,8 +4,17 @@ function check() {
     if (name == "1" && pass == "1") {
         alert("登入成功");
         window.document.f.action = "./Update/index.html";
-        window.document.f.submit();
-    } else {
-        alert("用户名或密码错误");
-    }
+        //window.document.f.submit();
+    } 
+    //else {
+      //  alert("用户名或密码错误");
+    //}
+    if (name == "2" && pass == "2") {
+        alert("登入成功");
+        window.document.f.action = "./Update/index.html";
+        //window.document.f.submit();
+    } 
+    //else {
+      //  alert("用户名或密码错误");
+    //}
 }
